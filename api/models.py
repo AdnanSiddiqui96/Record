66742eca7488edcc2e2b574a286a87071371750b
AdnanSiddiqui96 <adnannsiddiqui21@gmail.com>
2023-09-01 16:22:55 +0500

    done

---
+++ api/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+class Class(models.Model):
+
+    class_name = models.CharField(max_length=255, default='')
+
+    def __str__(self):
+        return self.class_name
+
+
+class student(models.Model):
+
+    fisrtname = models.CharField(max_length=255, default='')
+    lastname = models.CharField(max_length=255, default='')
+    email = models.EmailField(max_length=255, default='')
+    contact_number = models.CharField(max_length=255, default='')
+    address = models.TextField(default='')
+    classid = models.ForeignKey(Class, on_delete =models.CASCADE,blank=True, null=True,related_name="student_class")
+
+    def __str__(self):
+        return self.fisrtname + ' ' + self.lastname
+    
+class results(models.Model):
+
+        name = models.CharField(max_length=255, default='')
+        result = models.CharField(max_length=255, default='')
+
+        def __str__(self):
+            return self.name
